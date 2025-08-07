15145b97218b7dd7f7afcd46da76f7eeb3f8642a
Tommy0506 <2764393408@qq.com>
2025-08-07 22:53:09 +0800

    add CSRFProtect function and CSRFError

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask, render_template
+from flask_login import current_user
+from flask_wtf.csrf import CSRFError
+
 from bluelog.settings import Config
-from bluelog.extions import bootstrap, ckeditor, db, moment,login
+from bluelog.extions import bootstrap, ckeditor, db, moment,login, csrf_protect
 from bluelog.commands import register_command
-from bluelog.models import Admin, Category
+from bluelog.models import Admin, Category, Comment
+
 from blueprint.blog import bp as blog_bp
 from blueprint.admin import bp as admin_bp
 from blueprint.auth import auth_bp
@@ -11,6 +15,7 @@ from blueprint.auth import auth_bp
 def create_app(config_name=None):
 
     app = Flask(__name__)
+
 
     if config_name is None:
         config_name = os.getenv('FLASK_CONFIG', 'development')
@@ -22,32 +27,37 @@ def create_app(config_name=None):
         db.init_app(app)
         moment.init_app(app)
         login.init_app(app)
+        csrf_protect.init_app(app)
 
     def register_errors(app):
-        @app.errorhandler(400)
+        @app.errorhandler(CSRFError)
         def bad_request(e):
-            return render_template('error/400.html'),400
+            return render_template('error/400.html', description=e.description),400
 
     def register_template_context(app):
         @app.context_processor
         def template_context():
             admin = Admin.query.first()
             categories = Category.query.order_by(Category.name).all()
-            return dict(admin=admin, categories=categories)
+            if current_user.is_authenticated:
+                unread_comments = Comment.query.filter_by(review=False).count()
+            else:
+                unread_comments = None
+            return dict(admin=admin, categories=categories, unread_comments=unread_comments)
 
     def register_blueprint(app):
         app.register_blueprint(blog_bp)
         app.register_blueprint(admin_bp, url_prefix='/admin')
-        app.register_blueprint(auth_bp)
+        app.register_blueprint(auth_bp,url_prefix='/auth')
 
     register_extensions(app)
     register_command(app)
     register_errors(app)
     register_template_context(app)
     register_blueprint(app)
-    @app.route('/')
-    def hello_world():  # put application's code here
-        return '这是在工厂函数里注册的路由!'
+
+    login.login_view = 'auth.login'
+    login.login_message_category = 'warning'
 
     @app.shell_context_processor
     def make_shell_context():
--- bluelog/extions.py
+++ bluelog/extions.py
@@ -3,12 +3,15 @@ from flask_ckeditor import CKEditorField, CKEditor
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+
 
 bootstrap = Bootstrap4()
 ckeditor = CKEditor()
 db = SQLAlchemy()
 moment = Moment()
 login = LoginManager()
+csrf_protect = CSRFProtect()
 
 # 设置回调函数，使用flask-login验证用户登录状态时不仅返回用户id，而是返回用户模型类对象
 @login.user_loader
