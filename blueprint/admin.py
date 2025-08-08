55f4006578508970bc4646f481f91c3c4eeec859
Tommy0506 <2764393408@qq.com>
2025-08-08 21:17:38 +0800

    manage comments

--- blueprint/admin.py
+++ blueprint/admin.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, current_app, flash, redir
 from flask_login import login_required
 
 from bluelog.helpers import redirect_up
-from bluelog.models import Post, Category
+from bluelog.models import Post, Category, Comment
 from bluelog.extions import db
 from bluelog.forms import PostForm
 bp = Blueprint('admin',__name__)
@@ -74,4 +74,33 @@ def delete_post(post_id):
     db.session.commit()
     return redirect_up()
 
+@bp.route('/delete_comment/<int:comment_id>', methods=['POST'])
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect_up()
 
+@bp.route('/manage/comments')
+def manage_comments():
+    request_filter = request.args.get('filter','all')
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['BLUELOG_COMMENT_PER_PAGE']
+    if request_filter == 'Unread':
+        filter_comments = Comment.query.filter_by(review=False)
+    elif request_filter == 'from_admin':
+        filter_comments = Comment.query.filter_by(from_admin=True)
+    else:
+        filter_comments = Comment.query
+
+    pagination = filter_comments.order_by(Comment.timestamp.desc()).paginate(page=page, per_page=per_page)
+    comments = pagination.items
+    return render_template('admin/Manage_Comments.html', comments=comments, pagination=pagination)
+
+@bp.route('/approve/<int:comment_id>')
+def approve_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    comment.review=True
+    db.session.commit()
+    flash('Comment published','success')
+    return redirect_up()
